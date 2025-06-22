8f072ee9ead7132e99c67f3fd1c3b09b92b7a295
mnaibei <muchajulius@gmail.com>
2025-06-22 10:24:54 +0300

    Update and upgrade

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_12_115347) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_22_071224) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension "pg_catalog.plpgsql"
 
   create_table "logs", force: :cascade do |t|
     t.string "query"
@@ -28,5 +28,4 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_12_115347) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 end
